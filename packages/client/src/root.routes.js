0eee673e6cad6f95a2f5b6e659802cd64ca1c90c
xXValhallaCoderXx <renate.gouveia@gmail.com>
2022-09-26 07:07:07 +0800

    Catch all routes

--- packages/client/src/root.routes.js
+++ packages/client/src/root.routes.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { TaggedTxPage, DashboardPage } from "./pages";
 import AuthLayout from "./components/layout/CoreLayout.js";
 import LogoutModal from "./components/organisms/LogoutModal";
@@ -41,7 +41,7 @@ const RootRoutes = () => {
         <Route element={<AuthLayout />}>
           <Route path="/transactions/*" element={<DashboardPage />} />
           <Route path="/tagged-tx" element={<TaggedTxPage />} />
-          {/* <Route path="*" element={<Navigate to="/transactions" replace />} /> */}
+          <Route path="*" element={<Navigate to="/transactions" replace />} />
         </Route>
       </Routes>
       <LogoutModal
